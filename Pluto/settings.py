d113ae6919b2b994ee8f49657dd4849d9a5aa359
nikhilgupta18 <nikhilgupta1999@gmail.com>
2020-04-20 05:23:46 +0530

    uploaded

--- Pluto/settings.py
+++ Pluto/settings.py
@@ -128,4 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'mysite/static')
+# ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 django_heroku.settings(locals())
--- music/views.py
+++ music/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def Upload(request):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        url = fs.url(name)
+        context={
+            'url':url,
+        }
+    return render(request,'upload.html',context)
